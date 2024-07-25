07bf321812040ef7a225fec2220e4f6b18999e39
nguyengiale <nguyengiale.it@gmail.com>
2024-07-26 00:13:17 +0900

    first commit

---
+++ demo7.js
@@ -0,0 +1,7 @@
+function toggleMobileMenu() {
+  document.getElementById("mobileMenu").classList.toggle("hidden");
+  document.getElementById("mobileMenu").classList.toggle("flex");
+
+  document.getElementById("mobileMenuOverlay").classList.toggle("hidden");
+  document.getElementById("mobileMenuOverlay").classList.toggle("block");
+}
